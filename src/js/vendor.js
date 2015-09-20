493ed2aad0100e23ba724fb92df4a2e27a1662ee
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-09-20 19:46:12 +0300

    success end

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -4,7 +4,8 @@
 
 /* Third party */
 
+////= ../../bower_components/headjs/dist/1.0.0/head.js
 ////= ../../bower_components/jquery/dist/jquery.js
 ////= ../../bower_components/bootstrap/dist/js/bootstrap.js
-//= ../../bower_components/modernizr/modernizr.js
+////= ../../bower_components/modernizr/modernizr.js
 ////= ../../bower_components/css_browser_selector/css_browser_selector.js
